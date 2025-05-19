4b12271f3085ad3e8032c0f0379f9c14f154e9be
Alask-Code <anonimoplayer736@gmail.com>
2025-05-18 21:49:00 -0300

    feat: add user data and update API status endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,20 @@ const router = Router();
 
 router.get("/status", (req, res) => {
   res.json({
-    Uptime: process.uptime(),
+    description: "Endpoint destinado a verificar o estado da API.",
+    status: {
+      code: 200,
+      message: "API está funcionando corretamente.",
+      load: "10%",
+      uptime: "1h 30m",
+      endpoints: {
+        blockchain: "online",
+        users: "online",
+        transactions: "online",
+        wallets: "online",
+        home: "online",
+      },
+    },
   });
 });
 
